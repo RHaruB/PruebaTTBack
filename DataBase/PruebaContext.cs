66e626b59aa78b204102928ae419e91e715c5ec8
Haru Baldeon <40285629+htecnology@users.noreply.github.com>
2023-09-12 18:53:04 -0500

    Agregar archivos de proyecto.

---
+++ DataBase/PruebaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataBase
+{
+    public class PruebaContext : DbContext
+    {
+        public PruebaContext(DbContextOptions<PruebaContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Departamento> Departamentos { get; set;}
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ DataBase/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class Users
+    {
+        public  int id { set; get; }
+        public  string usuario { set; get; }
+        public  string primerNombre { set; get; }
+        public  string segundoNombre { set; get; }
+        public  string primerApellido { set; get; }
+        public  string segundoApellido { set; get; }
+        public  int idDepartamento { set; get; }
+        public  int idCargo { set; get; }
+    }
+}
